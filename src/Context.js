32bf793f64b019efd3ce660a8364df1ac2020ade
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-12-28 23:27:09 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,106 +1,75 @@
 import React, { Component } from "react";
 import { detailProduct, storeProducts } from "./data";
-import firebase from "firebase";
 
-
-
-
-
+import axios from "axios";
 
 import { toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
-
-
-
-
-
-
-
 const ProductContext = React.createContext();
 toast.configure();
 
 class ProductProvider extends Component {
-
-
-
-
   state = {
-     
     products: [],
     prod: [],
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
     modalProduct: detailProduct,
-    
-  
-    
+
+    todolist: [],
+
     cartSubTotal: 0,
     cartTax: 0,
     cartTotal: 0
   };
 
+  proceed = () => {
+    const mail = "akashvj";
 
+    let temp = [];
+    temp = [...this.state.cart];
+    temp.forEach((item) => {
+      const product = { ...item };
+      const it = {
+        title: product.title,
+        count: product.count,
+        email: mail
+      };
 
-  
-   
-      proceed = () => {
-        const user = firebase.auth().currentUser;
+      if (it.id) {
+        axios
+          .put(`http://127.0.0.1:8000/api/dbs/${it.id}/`, it)
+          .then((res) => this.refreshList());
+        return;
+      }
+      axios
+        .post("http://127.0.0.1:8000/api/dbs/", it)
+        .then((res) => this.refreshList());
 
-        const mail=user.email
-        
-        let temp=[]
-        temp=[...this.state.cart]
-        temp.forEach(item => {
-          const product = { ...item };
-    
-          const count=product.count
-          const title=product.title
-    
-        const db = firebase.firestore();
-        db.settings({
-          timestampsInSnapshots: true
-        });
-        
-        const userRef = db.collection("ecom1").add({
-          name: title,
-          count: count,
-          email: mail
-        });
-
-        this.setState({
-          name: "",
-          count: ""
-        });
-      
-   
-
-
+      this.setState({
+        title: "",
+        count: ""
+      });
     });
-  }
-  
-
-   
-
-    
-  
-  
-
-
-
-    
-
-    
+  };
 
   componentDidMount() {
     this.setProducts();
+    this.refreshList();
   }
+  refreshList = () => {
+    axios
+      .get("http://127.0.0.1:8000/8000/api/dbs/")
+      .then((res) => this.setState({ todoList: res.data }))
+      .catch((err) => console.log(err));
+  };
 
   setProducts = () => {
     let tempProducts = [];
-    storeProducts.forEach(item => {
+    storeProducts.forEach((item) => {
       const singleItem = { ...item };
       tempProducts = [...tempProducts, singleItem];
     });
@@ -109,18 +78,18 @@ class ProductProvider extends Component {
     });
   };
 
-  getItem = id => {
-    const product = this.state.products.find(item => item.id === id);
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
     return product;
   };
-  handleDetail = id => {
+  handleDetail = (id) => {
     const product = this.getItem(id);
     this.setState(() => {
       return { detailProduct: product };
     });
   };
 
-  addToCart = id => {
+  addToCart = (id) => {
     let tempProducts = [...this.state.products];
     const index = tempProducts.indexOf(this.getItem(id));
     const product = tempProducts[index];
@@ -139,7 +108,7 @@ class ProductProvider extends Component {
     );
   };
 
-  openModal = id => {
+  openModal = (id) => {
     const product = this.getItem(id);
     this.setState(() => {
       return { modalProduct: product, modalOpen: true };
@@ -151,9 +120,9 @@ class ProductProvider extends Component {
     });
   };
 
-  increment = id => {
+  increment = (id) => {
     let tempCart = [...this.state.cart];
-    const selectedProduct = tempCart.find(item => item.id === id);
+    const selectedProduct = tempCart.find((item) => item.id === id);
     const index = tempCart.indexOf(selectedProduct);
     const product = tempCart[index];
 
@@ -170,9 +139,9 @@ class ProductProvider extends Component {
     );
   };
 
-  decrement = id => {
+  decrement = (id) => {
     let tempCart = [...this.state.cart];
-    const selectedProduct = tempCart.find(item => item.id === id);
+    const selectedProduct = tempCart.find((item) => item.id === id);
 
     const index = tempCart.indexOf(selectedProduct);
     const product = tempCart[index];
@@ -192,11 +161,11 @@ class ProductProvider extends Component {
       );
     }
   };
-  removeitem = id => {
+  removeitem = (id) => {
     let tempProducts = [...this.state.products];
     let tempCart = [...this.state.cart];
 
-    tempCart = tempCart.filter(item => item.id !== id);
+    tempCart = tempCart.filter((item) => item.id !== id);
 
     const index = tempProducts.indexOf(this.getItem(id));
     let removedProduct = tempProducts[index];
@@ -229,7 +198,7 @@ class ProductProvider extends Component {
   };
   addTotals = () => {
     let subTotal = 0;
-    this.state.cart.map(item => (subTotal += item.total));
+    this.state.cart.map((item) => (subTotal += item.total));
     const tempTax = subTotal * 0.1; /*just taxt fr items */
     const tax = parseFloat(tempTax.toFixed(2));
     const total = subTotal + tax;
@@ -254,8 +223,7 @@ class ProductProvider extends Component {
           decrement: this.decrement,
           removeitem: this.removeitem,
           clearCart: this.clearCart,
-          proceed: this.proceed,
-          
+          proceed: this.proceed
         }}
       >
         {this.props.children}
